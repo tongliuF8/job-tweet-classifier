a95a3b0637759de1dc97583efb6a706b7d62bd35
Tong Liu <tongliu0314@gmail.com>
2018-09-18 22:47:18 -0400

    update model files

--- example.py
+++ example.py
@@ -1,29 +1,22 @@
 from sklearn.externals import joblib
-import timeit, math
 
 
 def main():
-    # mark the beginning time of process
-    start = timeit.default_timer()
 
-    vectorizer = joblib.load('vectorizer.pkl')
-    clf = joblib.load('f1_SVC.pkl')
+    vectorizer = joblib.load('model/C4_new_vectorizer.pkl')
+    clf = joblib.load('model/C4_new_f1_SVC.pkl')
 
-    message = 'dont put that on ur resume for a minimum wage job'
+    message1 = 'dont put that on ur resume for a minimum wage job'
+    message2 = 'hello world!'
 
-    test_X = vectorizer.transform([message])
-    y_pred_class = clf.predict(test_X)[0]
+    test_X = vectorizer.transform([message1, message2])
+    y_pred_class = clf.predict(test_X)
 
-    print(y_pred_class)
-
-    ##### mark the ending time of process #####
-    end = timeit.default_timer()
-    seconds = math.ceil(end - start)
-    # Convert Secs Into Human Readable Time String (HH:MM:SS)
-    m, s = divmod(seconds, 60)
-    h, m = divmod(m, 60)
-    print "This process took %d:%02d:%02d" % (h, m, s)
-
+    for pred in y_pred_class:
+        if pred == 1.0:
+            print('job')
+        else:
+            print('not job')
 
 if __name__ == '__main__':
     main()
